40836dd9e44f2d4e50a74bd68afbf2812e49ec75
dramenia <info@dramenia.nl>
2017-10-29 21:26:03 +0100

    small update

--- control_servo.py
+++ control_servo.py
@@ -1,10 +1,17 @@
+
+
 import RPi.GPIO as GPIO
 from time import sleep
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("echo", help="echo the string you use here")
-args = parser.parse_args()
+parser = argparse.ArgumentParser(
+    description="performs a variety of operations on a file.",
+    epilog="pretty neat, huh?",
+    fromfile_prefix_chars='@',
+)
+parser.add_argument("-A", "--angle", type=int, help="echo the string you use here")
+parser.add_argument("-O", "--orientation", help="echo the string you use here")
+parser.add_argument('-V', '--version', action="version", version="%(prog)s 0.1")
 
 def setup():
     GPIO.setmode(GPIO.BOARD)
@@ -14,16 +21,29 @@ def setup():
 
 
 def set_angle_x(angle):
-    duty = angle / 18 + 2
-    GPIO.output(13, True)
-    pwm = GPIO.PWM(13, 50)
-    pwm.ChangeDutyCycle(duty)
-    sleep(1)
-
-
-def set_angle_y(angle):
+    GPIO.setup(11, GPIO.OUT)
     duty = angle / 18 + 2
     GPIO.output(11, True)
     pwm = GPIO.PWM(11, 50)
-    pwm.ChangeDutyCycle(duty)
+    pwm.start(duty)
     sleep(1)
+    GPIO.cleanup()
+
+
+def set_angle_y(angle):
+    GPIO.setup(13, GPIO.OUT)
+    duty = angle / 18 + 2
+    GPIO.output(13, True)
+    pwm = GPIO.PWM(13, 50)
+    pwm.start(duty)
+    sleep(1)
+    GPIO.cleanup()
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    setup()
+    if args.orientation == "Y":
+        set_angle_y(args.angle)
+    else:
+        set_angle_x(args.angle)
